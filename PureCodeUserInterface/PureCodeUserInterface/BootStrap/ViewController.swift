7ca1f8e569c99ad3830ba371bad010500a146d2a
ericliuhusky <289471838@qq.com>
2021-07-13 16:02:50 +0800

    :sparkles: 纯代码UIKit

---
+++ PureCodeUserInterface/PureCodeUserInterface/BootStrap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PureCodeUserInterface
+//
+//  Created by lzh on 2021/7/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello UIKit!")
+    }
+
+
+}
+
